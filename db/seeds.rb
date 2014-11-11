976569fa22462d0198df9016682d994361d0cb51
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-11 09:20:00 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require '../spec/rails_helper'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = FactoryGirl.create(:user, email: "user1@example.com")
+user2 = FactoryGirl.create(:user, email: "user2@example.com")
+
+image_set = FactoryGirl.create(:image_set, user: user1)
+
+images = FactoryGirl.create_list(:image, image_set: image_set)
+
+comments = FactoryGirl.create_list(:comment, user: user1, image_set: image_set)
