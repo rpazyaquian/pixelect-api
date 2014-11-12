d9be4098a9e6be42586a93e87e82ce7b0b391d3c
CollinBlinder <collinblinder@gmail.com>
2014-11-11 21:43:03 -0500

    Recreated user authentication in a branch that is mergeable to the upstream.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resources :comments, except: [:new, :edit], defaults: { format: :json }
     resources :images, except: [:new, :edit], defaults: { format: :json }
   end
+
+    resources :users
+    resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
